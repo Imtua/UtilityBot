74ab3152322b0807019667c543e378c822491588
Imtua <itsololevelling@gmail.com>
2023-05-12 17:16:32 +0300

    Добавление контроллеров, написание инфраструктуры

--- Imtua.UtilityBot/Bot.cs
+++ Imtua.UtilityBot/Bot.cs
@@ -1,15 +1,60 @@
-﻿using Telegram.Bot;
+﻿using Imtua.UtilityBot.Controllers;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Imtua.UtilityBot
 {
-    internal class Bot
+    internal class Bot : BackgroundService
     {
         private ITelegramBotClient _telegramBotClient;
+        private DefaultMessageController _defaultMessageController;
+        private InlineKeyboardController _inlineKeyboardController;
+        private TextMessageController _textMessageController;
 
         public Bot(ITelegramBotClient telegramBotClient)
         {
             _telegramBotClient = telegramBotClient;
         }
 
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken ct)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _inlineKeyboardController.Handle(update.CallbackQuery, ct);
+            }
+            if (update.Type == UpdateType.Message)
+            {
+                if (update.Message.Type == MessageType.Text)
+                    await _textMessageController.Handle(update.Message, ct);
+
+                else
+                    await _defaultMessageController.Handle(update.Message, ct);
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken ct)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException 
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ =>exception.ToString()
+            };
+            Console.WriteLine(errorMessage);
+
+            return Task.CompletedTask;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramBotClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync,
+                new ReceiverOptions()
+                { AllowedUpdates = { } }, cancellationToken: stoppingToken);
+            Console.WriteLine("Бот запущен");
+        }
     }
 }
---
+++ Imtua.UtilityBot/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Imtua.UtilityBot.Controllers
+{
+    internal class InlineKeyboardController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Handle(CallbackQuery? callBackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Нажатие на кнопку от пользователя #{callBackQuery.From.Id} - {callBackQuery.From.FirstName} {callBackQuery.From.LastName}");
+            await _telegramClient.SendTextMessageAsync(callBackQuery.From.Id, $"Вы нажали на кнопку", cancellationToken: ct)
+        }
+    }
+}
