0162e12ea42704410c442bf9169fa59a4916918f
Imtua <itsololevelling@gmail.com>
2023-05-12 15:45:10 +0300

    Добавьте файлы проекта.

---
+++ Imtua.UtilityBot/Bot.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+
+namespace Imtua.UtilityBot
+{
+    internal class Bot
+    {
+        private ITelegramBotClient _telegramBotClient;
+
+        public Bot(ITelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient;
+        }
+
+    }
+}
