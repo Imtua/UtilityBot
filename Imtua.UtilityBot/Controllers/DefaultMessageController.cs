79f4c28d42866e716d12ec55deef6ed6df73d935
Imtua <itsololevelling@gmail.com>
2023-05-12 17:16:32 +0300

    Добавление контроллеров, написание инфраструктуры

---
+++ Imtua.UtilityBot/Controllers/DefaultMessageController.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Imtua.UtilityBot.Controllers
+{
+    internal class DefaultMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public DefaultMessageController(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Пользователь #{message.Chat.Id} - {message.Chat.FirstName} {message.Chat.LastName}" +
+                $" пытается отправить сообщение неподдерживаемого типа");
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Неправильный ввод", cancellationToken: ct);
+        }
+    }
+}
---
+++ Imtua.UtilityBot/Controllers/TextMessageController.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Imtua.UtilityBot.Controllers
+{
+    internal class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public TextMessageController(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Получено сообщение от пользователя #{message.Chat.Id} - {message.Chat.FirstName} {message.Chat.LastName}:\n" +
+                $"'{message.Text}'");
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено сообщение", cancellationToken: ct);
+        }
+    }
+}
